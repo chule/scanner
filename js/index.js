c7039b33953f6659389ea07cea0f374aa448198f
chule <cuckovic@gmail.com>
2015-08-14 13:53:22 +0200

    first

--- js/index.js
+++ js/index.js
@@ -61,14 +61,20 @@ var app = {
             alert("We got a barcode\n" + 
             "Result: " + result.text + "\n" + 
             "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
+            "Cancelled: " + result.cancelled); 
 
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
+
+             
+
+           // console.log("Scanner result: \n" +
+           //      "text: " + result.text + "\n" +
+           //      "format: " + result.format + "\n" +
+           //      "cancelled: " + result.cancelled + "\n");
+
+            // document.getElementById("info").innerHTML = result.text;
+
+
+            //console.log(result);
             /*
             if (args.format == "QR_CODE") {
                 window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
