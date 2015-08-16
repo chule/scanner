ab7f0bd69b1d92c1c7f2fc6196f34f5fc9350ffe
igor <cuckovic@gmail.com>
2015-08-16 21:55:23 +0200

    alert

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.getElementById('scan').addEventListener('click', this.scan, false);
-        //document.getElementById('alert').addEventListener('click', this.alert, false);
+        document.getElementById('alert').addEventListener('click', this.alert, false);
         document.getElementById('encode').addEventListener('click', this.encode, false);
     },
 
@@ -38,9 +38,25 @@ var app = {
         console.log('Received Event: ' + id);
     },
 
-    // alert: function () {
-    //     alert("Root sustave je" + cordova.file.dataDirectory); //
-    // },
+    alert: function () {
+        //alert("Root sustave je" + cordova.file.dataDirectory); //
+
+        myFile1 = "myReadme.txt";
+
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+
+        function gotFS(fileSystem) {
+            fileSystem.root.getFile(myFile1, {create: true, exclusive: false}, gotFileEntry, fail);
+        }
+
+        function gotFileEntry(fileEntry) {
+            alert("good");                      
+        }
+
+        function fail(error) {
+            alert("Error");
+        }        
+    },
 
     scan: function() {
         
